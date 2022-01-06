661ca190e564cd31110a16c5247caa14ac37f71d
Claire Royer <claireroyer57@gmail.com>
2022-01-06 22:54:09 +0100

    Create backend directory

---
+++ back/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Allow application to acces API with no problem.
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow acces to everybody.
+  res.setHeader(
+    "Access-Control-Allow-Headers", // Authorize to use hearder mentionned on object req.
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS" // Authorize to send methods mentionned.
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Message console");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Test" });
+  next();
+});
+
+module.exports = app;

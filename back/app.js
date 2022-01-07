3efd020473152ffa47801fc4c3a4ad84b78143ef
Claire Royer <claireroyer57@gmail.com>
2022-01-07 21:43:36 +0100

    Add mongoose

--- back/app.js
+++ back/app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://bipce:<password>@cluster0.lg26b.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 

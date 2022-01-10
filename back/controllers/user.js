17af612f4c8f63816cc8b31e2868bb64084591bb
Bipce <claireroyer57.com>
2022-01-10 02:33:14 +0100

    Add function login in ctrl file

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -4,14 +4,13 @@ const User = require("../models/user");
 
 // Register new user
 exports.signup = async (req, res, next) => {
-  let hash = "";
+  let hash;
   try {
     hash = await bcrypt.hash(req.body.password, 10);
   } catch (error) {
     res.status(500).json({ error });
   }
   try {
-    console.log(hash);
     const user = new User({ email: req.body.email, password: hash });
     await user.save();
     res.status(201).json({ message: "Utilisateur crée !" });
@@ -20,21 +19,39 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-// exports.signup = (req, res, next) => {
-//   bcrypt
-//     .hash(req.body.password, 10)
-//     .then((hash) => {
-//       const user = new User({
-//         email: req.body.email,
-//         password: hash,
-//       });
-//       user
-//         .save()
-//         .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-//         .catch((error) => res.status(400).json({ error }));
+// Connect existing user.
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(401).json({ error: "Utilisateur non trouvé !" });
+    const valid = await bcrypt.compare(req.body.password, user.password);
+    if (!valid)
+      return res.status(401).json({ error: "Mot de passe incorrect !" });
+    res.status(200).json({ userId: user._id, token: "TOKEN" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+// .then .catch
+// exports.login = (req, res, next) => {
+//   User.findOne({ email: req.body.email })
+//     .then((user) => {
+//       if (!user) {
+//         return res.status(401).json({ error: "Utilisateur non trouvé !" });
+//       }
+//       bcrypt
+//         .compare(req.body.password, user.password)
+//         .then((valid) => {
+//           if (!valid) {
+//             return res.status(401).json({ error: "Mot de passe incorrect !" });
+//           }
+//           res.status(200).json({
+//             userId: user._id,
+//             token: "TOKEN",
+//           });
+//         })
+//         .catch((error) => res.status(500).json({ error }));
 //     })
 //     .catch((error) => res.status(500).json({ error }));
 // };
-
-// Connect existing user.
-exports.login = (req, res, next) => {};
